700e20cd1fdabe3e51fe9d4b2395667db15ddc6e
kristi8 <qristina.chelidze@gmail.com>
2022-11-30 23:17:22 +0400

    forms validation

--- script.js
+++ script.js
@@ -33,92 +33,142 @@ let sliderContent = document.getElementById("slider-content");
 let sliderIndex = 0;
 
 // დივ ტეგი
-function createDiveTag(){
-const divTag = document.createElement('div');
-divTag.classList.add('slide');
+function createDiveTag() {
+  const divTag = document.createElement("div");
+  divTag.classList.add("slide");
 
-return divTag;
+  return divTag;
 }
 
 // სურათი
 // image-item რაც გვინდა იმას დავარქმევთ
 
-function creteImgtag (image) {
-    // let tagImage = document.createElement('img');
-    // tagImage.setAttribute('src', image.imageUrl);
-    // tagImage.setAttribute('alt', image.title);
+function creteImgtag(image) {
+  // let tagImage = document.createElement('img');
+  // tagImage.setAttribute('src', image.imageUrl);
+  // tagImage.setAttribute('alt', image.title);
 
-    // bg.image
-    let tagImage = document.createElement('div');
-    tagImage.style.backgroundImage = `url(${image.imageUrl})`;
-    tagImage.classList.add('bg-image');
+  // bg.image
+  let tagImage = document.createElement("div");
+  tagImage.style.backgroundImage = `url(${image.imageUrl})`;
+  tagImage.classList.add("bg-image");
 
-
-    return tagImage;
+  return tagImage;
 }
 // სათაური
-function createTitletag(image){
-    let tagTitle = document.createElement('h3');
-    tagTitle.textContent = image.title;
+function createTitletag(image) {
+  let tagTitle = document.createElement("h3");
+  tagTitle.textContent = image.title;
 
-    return tagTitle;
+  return tagTitle;
 }
 
 // dot შექმნა
-function createDots(){
-    let dotsParent = document.createElement('div');
-    dotsParent.classList.add('dotParent');
+function createDots() {
+  let dotsParent = document.createElement("div");
+  dotsParent.classList.add("dotParent");
 
-    data.forEach(element =>{
-        let dot = document.createElement('div');
-        dot.classList.add('dot');
-        dotsParent.appendChild(dot);
-    })
+  data.forEach((element) => {
+    let dot = document.createElement("div");
+    dot.classList.add("dot");
+    dotsParent.appendChild(dot);
+  });
 
-    return dotsParent;
+  return dotsParent;
 }
 function slide() {
-    sliderContent.innerHTML = " "; 
-    let slideItem = createDiveTag(data[sliderIndex]);
-    let imgTag = creteImgtag(data[sliderIndex]);
-    let titleTag = createTitletag(data[sliderIndex]);
-    let dotsElement = createDots();
-    
-    slideItem.appendChild(imgTag);
-    slideItem.appendChild(titleTag);
-    sliderContent.appendChild(slideItem);
-    sliderContent.appendChild(dotsElement);
+  sliderContent.innerHTML = " ";
+  let slideItem = createDiveTag(data[sliderIndex]);
+  let imgTag = creteImgtag(data[sliderIndex]);
+  let titleTag = createTitletag(data[sliderIndex]);
+  let dotsElement = createDots();
 
+  slideItem.appendChild(imgTag);
+  slideItem.appendChild(titleTag);
+  sliderContent.appendChild(slideItem);
+  sliderContent.appendChild(dotsElement);
 }
 function arroLeftclick() {
-    if (sliderIndex == 0){
-        // sliderIndex = data.length - 1;
-        // slide()
-        return;
-    }
-    sliderIndex--;
-    slide();
+  if (sliderIndex == 0) {
+    // sliderIndex = data.length - 1;
+    // slide()
+    return;
+  }
+  sliderIndex--;
+  slide();
 }
-function arrowRightclick(){
-    if(sliderIndex == data.length -1){
-        // sliderIndex = 0;
-        // slide()
-        return;
-    }
-    sliderIndex++;
-    slide();
+function arrowRightclick() {
+  if (sliderIndex == data.length - 1) {
+    // sliderIndex = 0;
+    // slide()
+    return;
+  }
+  sliderIndex++;
+  slide();
 }
 
-arrowLeft.addEventListener('click',arroLeftclick);
+arrowLeft.addEventListener("click", arroLeftclick);
 
+arrowRight.addEventListener("click", arrowRightclick);
 
-arrowRight.addEventListener('click', arrowRightclick);
-
-setInterval( () => {
-    arrowRightclick()
-},3000
-
-)
+setInterval(() => {
+  arrowRightclick();
+}, 3000);
 
 slide();
+
+// forms validation
+let registrationForm = document.getElementById("registrationForm");
+
+registrationForm.addEventListener("submit", function (event) {
+  event.preventDefault();
+  let errors = {};
+
+  // username
+  let usernameValue = document.getElementById("usernameField").value;
+  if (usernameValue == "") {
+    errors.username = "Username filed can not be empty";
+  }
+
+  // passvord
+
+  let passwordValue = document.getElementById("passwordField").value;
+  let passwordValue2 = document.getElementById("passwordFieldrepeat").value;
+
+  if (passwordValue == "") {
+    errors.password = "Password filed can not be empty";
+  }
+
+  if (passwordValue != passwordValue2) {
+    errors.password2 = "Passwords do not match";
+  }
+
+  // checkbox
+
+  let agreeField = document.getElementById("agreeTerms").checked;
+
+  if (!agreeField) {
+    errors.agree = "You must agree our terms and conditions";
+  }
+
+  console.log(errors);
+
+  document.querySelectorAll(".error-text").forEach(item1=> {
+    item1.innerText =" ";
+  });
+
+  for (let key in errors) {
+    console.log(key);
+    let spanText = document.getElementById('error_' + key);
+
+    if (spanText) {
+      spanText.innerText = errors[key];
+    }
+  }
+
+  if(Object.keys(errors).length == 0){
+    registrationForm.submit();
+  }
+});
+
 
